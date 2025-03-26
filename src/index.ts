934b01a999fa031581f87a8b1169e0c95ee6782c
Ryo Okubo <syucream1031@gmail.com>
2025-03-27 01:30:45 +0900

    stop noisy logging:

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import type { paths } from './api.js';
 export type { paths } from './api.js';
 
 export const createLightdashClient = (baseUrl: string, options?: RequestInit) => {
-  console.log('initializing lightdash client...');
-  console.log('baseUrl', baseUrl);
   return createClient<paths>({ baseUrl, ...options });
 };
 
